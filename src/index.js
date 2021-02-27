d91beeb9d9e159fefb82943262e871e9a2d6355f
Sergey Syleimanov <ssuleimanov.94@gmail.com>
2021-02-27 23:50:17 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let num = String(n).split("");
+    if (num[0] === '-') {
+        num.shift();
+        return Number(num.reverse().join(""))
+    } else {
+        return Number(num.reverse().join(""))
+    }
 }
